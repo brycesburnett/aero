c368ec03b5511c0585674ccdd415f4d9d097fcb2
marvintrajano <marvin.trajano@gmail.com>
2014-09-23 17:55:39 -0700

    Create Parametric Cubic Spline.py

---
+++ Parametric Cubic Spline.py
@@ -0,0 +1,23 @@
+  PI_RAD = 3.14159
+TWO_PI = 2 * PI_RAD
+RqD = PI_RAD / 180
+
+  
+def single_curve_airfoil():
+
+def tau_chi(tau, deLta, chi, chi0, chid, chidd):
+
+def cubic_spline(nn, xx(), yy(), keL, dydxL, keR, dydxR, Xa, Xb, Xo, Yo, dydx(), zz(), area):
+
+def CS_solve(nn, xx(), yy(), zz(), dydx(), keL, dydxL, keR, dydxR):
+  
+def CS_quadr(nn, xx(), yy(), zz(), dydx(), Xa, Xb, area):
+
+def CS_intrp(nn, xx(), yy(), zz(), dydx(), Xo, Yo, dydxo, d2ydx2o):
+
+def Gauss(n, A(), L(), s()):
+  
+def Solve(n, A(), L(), B(), x()):
+
+def polynomial_(Left_to_Right, n, X_(), Y_(), c()):
+
