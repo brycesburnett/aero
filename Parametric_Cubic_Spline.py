35a556feeb477ca5548a5ff9de532d171ed8f9dc
marvintrajano <marvin.trajano@gmail.com>
2014-10-08 02:02:59 -0700

    Working parametric cubic spline
    Here's the working parametric cubic spline, it prints out the two equations needed for blender. (Values are hard coded for testing purposes)

--- Parametric_Cubic_Spline.py
+++ Parametric_Cubic_Spline.py
@@ -1,393 +1,71 @@
 import math
-PI_RAD = 3.14159
-TWO_PI = 2 * PI_RAD
-RqD = PI_RAD / 180
+import numpy as np
 
-#lists to be used
-c = []
-X_ = []
-Y_ = []
-tau_S = []
-chi_S = []
-zet_S = []
-dzdt_S = []
-d2zdt2_S = []
-dydx = []
-zz = []
-_del = []
-eps = []
-BB = []
-AA = []	#declaring as 1d but can assign 2D properties later
-dydxo = 0
-dydxL = 0
-dydxR = 0
+delta = 0.05 #shifts initial max thickness aft
+PIRAD = 3.14159
+TWOPI = 2 * PIRAD
+RqD = PIRAD / 180
 
-#main function
-def single_curve_airfoil():
-	deLta = 0.05
-	#3 point design
-	#tau_S = []
-	#chi_S = []
-	chi = None
-	chi0 = None
-	chid = None
-	chidd = None
-	#hard coded "user inputs"
-	tau_S.append(0.0000)
-	tau_S.append(0.0300)
-	tau_S.append(0.1900)
-	tau_S.append(0.5000)
-	tau_S.append(0.8800)
-	tau_S.append(1.0000)
-	#tau_chi(tau_S, deLta, chi_S, chi0, chid, chidd)
-	#zet_S = []
-	zet_S.append(0.0000)
-	zet_S.append(0.0007)
-	zet_S.append(-0.049)
-	zet_S.append(0.0000)
-	zet_S.append(0.0488)
-	zet_S.append(0.0000)
-	
-	Np = 6 #Number of parameters
-	
-	#generate resulting curves and get derivatives + normals
-	keL = 3
-	keR = 1
-	#dzdt_S = []
-	#d2zdt2_S = []
-	print "TAU_S: ", len(tau_S)
-	print "zet_S: ", len(zet_S)
-	CS_solve(Np, tau_S, zet_S, d2zdt2_S, dzdt_S, keL, dydxL, keR, dydxR)
-	
-	#get coefficients for degree-n polynomial passing thru spline points
-	Left_to_Right = 1
-	n = Np - 1
-	#c = []
+Np = 6 #number of points
+tau_S = np.array(np.zeros(Np)) #plus one because 1 to n is preferred
+zet_S = np.array(np.zeros(Np))
+chi_S = np.array(np.zeros(Np))
 
-	for i in range(0,n):
-		X_[i] = tau_S[i+1]
-		Y_[i] = zet_S[i+1]
-	polynomial_(Left_to_Right, n, X_, Y_, c)
-	
-	ns = 41
-	for i in range(1,ns):
-		irow = i+1
-		tau = (i-1) / (ns - 1)
-		tau_chi(tau, deLta, chi, chi0, chid, chidd)
-		CS_intrp(Np, tau_S, zet_S, d2zdt2_S, dzdt_S, tau, zeta, dzdt, d2zdt2)
-		Xo = X_[0]
-		Xn = X_[n]
-		Yo = Y_[0]
-		Yn = Y_[n]
-		x = tau
-		if(Left_to_Right == 1):
-			y = Yo
-			for j in range(1,n):
-				y = y + c[j] * (x - Xo) ^ j
-		else:
-			y = Yn
-			for j in range(1,n):
-				y = y + c[j] * (x - Xn) ^ j			
-		zetap = y
-		
-		#Leading edge radius and boattail
-		if(tau == 0):
-			betad = -(1 / RqD) * dzdt / chid
-		elif(tau == 0.5):
-			rho = dzdt ^ 2 / chidd 
-		elif(tau == 1):
-			betad = -(1 / RqD) * dzdt / chid
+#these points are USER INPUTS
+tau_S[0] = 0.00
+tau_S[1] = 0.03
+tau_S[2] = 0.19
+tau_S[3] = 0.50
+tau_S[4] = 0.88
+tau_S[5] = 1.00
 
-def tau_chi(tau, deLta, chi, chi0, chid, chidd):
-	chi0 = 1 - math.sin(PI_RAD * tau) 
-	
-	chi = 1 - ((1 - deLta) * math.sin(math.pi * tau) + 
-	deLta * math.sin(3 * PI_RAD * tau))
-	
-	chid = -((1 - deLta) * PI_RAD * math.cos(PI_RAD * tau) + 
-	3 * PI_RAD * deLta * math.cos(3 * PI_RAD * tau))
-	
-	chidd = ((1 - deLta) * (math.pi**2) * math.sin(math.pi * tau) - 
-	((3 * math.pi)**2) * deLta * math.sin(3 * math.pi * tau))	
-	
+zet_S[0] = 0.00
+zet_S[1] = 0.0007
+zet_S[2] = -0.049
+zet_S[3] = 0.00
+zet_S[4] = 0.0488
+zet_S[5] = 0.00
 
-def cubic_spline(nn, xx, yy, keL, dydxL, keR, dydxR, Xa, Xb, Xo, Yo, dydx, zz, area):
-	CS_solve(nn, xx, yy, zz, dydx, keL, dydxL, keR, dydxR)
-	CS_quadr(nn, xx, yy, zz, dydx, Xa, Xb, area)
-	CS_intrip(nn, xx, yy, zz, dydx, Xo, Yo, dydxo, d2ydx2o)
+Left_to_Right = 1
+n = Np - 1
+c = np.array(np.zeros(n))
+X = np.array(np.zeros(Np))
+Y = np.array(np.zeros(Np))
 
+for i in range(0,Np):
+    X[i] = tau_S[i]
+    Y[i] = zet_S[i]
 
-def CS_solve(nn, xx, yy, zz, dydx, keL, dydxL, keR, dydxR):
-	if(keL==2):
-		dydxL = 0
-	if(keR==2):
-		dydxR = 0
-	#1st and 2nd derivative arrays
-  	#dydx = []
-  	#zz = []
-  	#number of splines
+#call polynomial
+Xo = X[0]
+Yo = Y[0]
+Xn = X[n]
+Yn = Y[n]
 
-  	ns = nn-1
-  	_del = [0] * (ns)
-  	esp = [0] * (ns)
-  	#_del = []
-  	#eps = []
-  	#spline length and height
-  	for i in range(1, ns):
-  		print i
-  		_del[i] = xx[i+1] - xx[i]
-  		esp[i] = yy[i+1] - yy[i]
-  	#generate 2D array [A][X]=[B]
-  	AA = [[0 for i in xrange(nn)] for i in xrange(nn)]
-  	BB = [0]*nn
-  	#BB = []
-  	#avoid user error for edge conditions
-  	if((keL < 1 or keL > 3) or (nn < 4 and keL == 3)):
-  		keL = 1
-  	if((keR < 1 or keR > 3) or (nn < 4 and keR == 3)):
-  		keR = 1
-  	#if(nn < 4 and keL == 3):
-  	#	keL = 1
-  	#if(nn < 4 and keR == 3):
-  	print "DEL: ", _del
-  	print "ESP: ", esp
-  	#influence coefficient matrix, row i column j
-  	for i in range(1, nn):
-  		for j in range(1, nn):
-  			#init 2d array with 0's as we traverse
-  			#(should already have em but lets be safe!)
-  			#AA[i][j] = 0
-  			#1st row (L-edge) stiff end
-  			if(keL == 1 and i == 1 and j == 1):
-  				#if he's hard coding the logic we could just say AA[1][1] here
-  				AA[i][j] = 1
-  			#1st row (L-edge) horizontal end
-  			elif((keL == 0 or keL == 2) and (i == 1 and j == 1)):
-  				#here too
-  				AA[i][j] = _del[1] / 3
-  			elif((keL == 0 or keL == 2) and (i == 1 and j == 2)):
-  				AA[i][j] = _del[1] / 6
-  			#1st row (L-edge) flexible end
-  			elif(keL == 3 and i == 1 and j == 1):
-  				AA[i][j] = 1
-  			elif(keL == 3 and i == 1 and j == 2):
-  				AA[i][j] = -1 - _del[1] / _del[2]
-  			elif(keL == 3 and i == 1 and j == 3):
-  				AA[i][j] = _del[1]/_del[2]
-  			#mid row (all options)
-  			elif(i > 1 and i < nn):
-  				if(j == i-1):
-  					AA[i][j] = _del[i-1] / 6
-  				elif(j == i):
-  					print "J: ", j, "\nI: ",i
-  					print AA[i][j]
-  					print _del[i-1]
-  					print "AA: ", AA
-  					AA[i][j] = (_del[i-1] + _del[i]) / 3
-  				elif(j == i + 1):
-  					AA[i][j] = _del[i] / 6
-  			#nth row (R-edge) stiff end
-  			elif(keR == 1 and i == nn and j == nn):
-  				AA[i][j] = 1
-  			#nthr row (R-edge) horizontal end
-  			elif((keR == 0 or keR == 2) and i == nn and j == nn - 1):
-  				AA[i][j] = -_del[ns] / 6
-  			elif((keR == 0 or keR == 2) and i == nn and j == nn - 0):
-  				AA[i][j] = -_del[ns] / 3
-  			#nth row (R-edge) flexible end
-  			elif(keR == 3 and i == nn and j == nn):
-  				AA[i][j] = 1
-  			elif(keR == 3 and i == nn and j == nn - 1):
-  				AA[i][j] = -1 - _del[nn-1] / _del[nn-2]
-  			elif(keR == 3 and i == nn and j == nn - 2):
-  				AA[i][j] = _del[nn-1] / _del[nn-2]
-  		#init boundary conditions
-  		BB[i]=0
-  		#1st row (L-edge) stiff end
-  		if(keL == 1 and i == 1):
-  			BB[i] = 0
-  		#1st row (L-edge) horizontal end
-  		elif((keL == 0 or keL == 2) and i == 1):
-  			BB[i] = esp[1] / _del[1] - dydxL
-  		#1st row (L-edge) flexible end
-  		elif(keL == 3 and i == 1):
-  			BB[i] = 0
-  		#mid row (all options)
- 		elif(i > 1 and i < nn):
- 			print "here", esp[i],_del[i]
- 			BB[i] = (esp[i] / _del[i]) - (esp[i-1] / _del[i-1])
- 		#nth row (R-edge) stiff end
- 		elif(keR == 1 and i == nn):
- 			BB[i] = 0
- 		#nth row (R-edge) horizontal end
- 		elif((keR == 0 or keR == 2) and i == nn):
- 			BB[i] = esp[ns] / _del[ns] - dydxR
- 		#nth row (R-edge) flexible end
- 		elif(keR == 3 and i == nn):
- 			BB[i] = 0
- 	#solution for 2nd derivatives, includes aux arrays
- 	Gauss(nn, AA, iL, ss)
- 	Solve(nn, AA, iL, BB, zz)
+A = np.matrix(np.zeros((n,n)))
+B = np.matrix(np.zeros((n,1)))
 
- 	#1st derivateves
- 	for i in range(1, nn):
- 		if(i < nn):
- 			dydx[i] = eps[i] / _del[i] - zz[i] * _del[i] / 3 - zz[i + 1] * _del[i] / 6
- 		else:
- 			dydx[i] = dydx[ns] + zz[ns] * _del[ns] / 2 + zz[nn] * _del[ns] / 2
+for i in range(0, n):
+    if(Left_to_Right == 1):
+        B[i] = Y[i+1] - Yo
+    else:
+        B[i] = Y[n-i] - Yn
+    for j in range(0, n):
+        if(Left_to_Right == 1):
+            A[i,j] = (X[i+1] - Xo) ** (j+1)
+        else:
+            A[i,j] = (X[n] - Xn) ** (j+1)
 
-  
-def CS_quadr(nn, xx, yy, zz, dydx, Xa, Xb, area):
-	
-	ns = nn - 1
-	
-	
-	#Spline length and height
-	for i in range (i, ns):
-		_del.insert(i, xx[i + 1] - xx[i])
-		eps.insert(i, yy[i + 1] - yy[i])
-		
-		#Initialize
-		area = 0
-		
-		#Begin with full splines
-		for i in range (i, ns):
-			if (Xa <= xx[i] and Xb >= xx[i + 1]):
-				area = (area +
-				yy[i] * _del[i] + dydx[i] * ((_del[i]**2)/2) +
-				zz[i] * ((_del[i]**3)/6) +
-				(zz[i+1] - zz[i]) * ((_del[i]**3)/24))
-		
-			#Partial spline, xa
-			if (Xa > xx[i] and Xa < xx[i + 1]):
-				area = (area +
-				yy[i] * _del[i] + dydx[i] * ((_del[i]**2)/2) +
-				zz[i] * ((_del[i]**3)/6) +
-				(zz[i + 1] - zz[i]) * ((_del[i]**3)/24) -
-				yy[i] * (Xa - xx[i]) + dydx[i] * (((Xa - xx[i])**2)/2) -
-				zz[i] * (((Xa - xx[i])**3)/6) -
-				(zz[i + 1] - zz[i]) * ((Xa - xx[i])**4)/(24 * _del[i]))
- 
-			#Partial spline, xb
-			if (Xb > xx[i] and Xb < xx[i + 1]):
-				area = (area +
-				yy[i] * (Xb - xx[i]) + dydx[i] * (((Xb - xx[i])**2)/2) +
-				zz[i] * (((Xb - xx[i])**3)/6) +
-				(zz[i + 1] - zz[i]) * (((Xb - xx[i]**4))/2))
-			
-			#Correct for overlap condition
-			if Xa >= xx[i] and Xb <= xx[i + 1]:
-				area = (area -
-				yy[i] * _del[i] + dydx[i] * ((_del[i]**2)/2) -
-				zz[i] * ((_del[i]**3)/6) -
-				(zz[i + 1] - zz[i]) * ((_del[i]**3)/24))
+#call gauss and solve
 
-			#Extrapolate left and/or right
-			if Xa < xx[i]:
-				area = area + (xx[1] - Xa) * 0.5 * (2 * yy[1] - dydx[1] * (xx[1] - Xa))
-				
-			if Xb > xx[nn]:
-				area = area + (Xb - xx[nn]) * 0.5 * (2 * yy[nn] + dydx[nn] * (Xb - xx[nn]))	
-	
-	
-def CS_intrp(nn, xx, yy, zz, dydx, Xo, Yo, dydxo, d2ydx2o):
+coefficient = np.linalg.solve(A,B)
+y_equation = "1-(1-"+str(delta)+")*sin(pi*u)+"+str(delta)+"*sin(3*pi*u)"
 
-	# Cubic spline interpolation module
-	# Interp. Yo(Xo) & get dy/dx, d2y/dx2 at Xo, incl. linear extrap. if req'd
-	# All above are inputs, except the results: (Yo, dydxo, & d2ydx2o)
-	# Phil Barnes, Feb 2009, Public Domain, www.HowFliesTheAlbatross.com
-	ns = nn - 1 
-	#ns = number of splines for nn points
-	# spline horizontal and vertical excursions:
-	for i in range (1, ns):
-		_del[i] = xx[i+1] - xx[i]
-		esp[i] = yy[i+1] - yy[i]
+z_equation = ""
+for i in range(1,n+1):
+    z_equation = z_equation + str(coefficient.item(i-1)) + "*u**"+str(i)
+    if(i != n):
+        z_equation = z_equation + "+"
 
-	if(Xo < xx[1]):
-	# linear extrapolate left
-	    Yo = yy[1] + dydx[1] * (Xo - xx[1])
-	    dydxo = dydx[1] + zz[1] * (Xo - xx[1])
-	    d3ydx3 = (zz[2] - zz[1]) / _del[1]       # 3rd derivative
-	    d2ydx2o = zz[1] + d3ydx3 * (Xo - xx[1])
-	elif(Xo > xx[nn]):
-	#linear extrapolate right
-		Yo = yy[nn] + dydx[nn] * (Xo - xx[nn])
-		dydxo = dydx[nn] + zz[nn] * (Xo - xx[nn])
-		d3ydx3 = (zz[nn] - zz[nn - 1]) / _del[ns]  # 3rd derivative
-		d2ydx2o = zz[1] + d3ydx3 * (Xo - xx[1])
-	else: # find applicable spline and interpolate
-		for i in range (1, ns):
-			if(xx[i + 1] >= Xo):
-				xmxi = (Xo - xx[i])
-				epsi = eps[i]; deli = _del[i];
-				yyi = yy[i]; zzi = zz[i]
-				dydxi = dydx[i]; zip1 = zz[i + 1]
-				Yo = yyi + dydxi * xmxi + zzi * xmxi ** 2 / 2 + (zip1 - zzi) * xmxi ** 3 / (6 * deli)
-				dydxo = dydxi + zzi * xmxi + (zip1 - zzi) * xmxi ** 2 / (2 * deli)
-				d2ydx2o = zzi + (zip1 - zzi) * xmxi / deli
-
-
-def Gauss (n, A, L, s):
-	for i in range (1,n):
-		L[i] = i
-		smax = 0
-		for j in range (1,n):
-			if (math.abs(A[i,j]) > smax):
-				smax = math.abs(A[i,j])
-		s[i] = smax
-		
-	for k in range(1,n):
-		rmax = 0
-		for i in range(k,n):
-			R = (math.abs(A[L[i],k]/s[L[i]]))
-			if (R > rmax):
-				j = i
-				rmax = R
-		Lk = L[j]
-		L[j] = L[k]
-		L[k] = Lk
-		for i in range(k+1,n):
-			xm = (A[L[i],k])/A[Lk,k]
-			for j in range(k+1,n):
-				A[L[i],j] = A[L[i],j] - xm*A[Lk,j]
-			A[L[i],k] = xm
-  
-def Solve (n, A, L, B, x):
-	for k in range(1,n-1):
-		for i in range(k+1,n):
-			B[L[i]] = (B[L[i]] - A[L[i],k] * B[L[k]])
-	x[n] = B[L[n]] / A[L[n],n]
-	for i in range(n-1,1,-1):
-		Sum = B[L[i]]
-		for j in range(i+1,n):
-			Sum = (Sum - (A[L[i],j]*x[j]))
-		x[i] = Sum / A[L[i],i]
-	for k in range(0, len(x)):
-		print x[i]
-
-def polynomial_(Left_to_Right, n, X_, Y_, c):
-	Xo = X_[0]; Yo = Y_[0]; Xn = X_[n]; Yn = Y_[n]
-	#
-	# setup & tab linear-algebra [A][c]=[b] to solve for poly. coef. (c)
-	#  ReDim A[n, n], c[n], B[n]
-	# ReDim either deallocates unused array space or adds more but since
-	# python lists are mutable, we don't have to use ReDim
-	for i in range(1, n):   # row
-		if Left_to_Right == 1:
-			B[i] = Y_[i] - Yo
-		else: # take deltaY from right-hand end:
-			B[i] = Y_[n - i] - Yn
-		# diagnostic print:
-		# Cells(1 + i, 18) = B(i) selected column assumes max n = 10
-		for j in range(1,n):  # column
-			if Left_to_Right == 1: 
-				A[i,j] = (X_[i] - Xo) ** j
-			else: # take deltaX from right-hand end:
-				A[i, j] = (X_[n-i] - Xn) ** j
-		# diagnostic print:
-		# Cells(1 + i, 7 + j) = A(i, j)
-	# solve for coefficients
-	#  ReDim iL[n], ss[n]; 
-	Gauss(n, A, iL, ss); Solve(n, A, iL, B, c)
-
-single_curve_airfoil()
+print(z_equation)
+print(y_equation)
