8b0c9776171217d30b2f90d90d61fc78999e9f20
Benjamin Kray <benkray@gmail.com>
2014-10-09 07:53:25 -0700

    added new property to wing
    now creates 2 wings, one mirrored over x axis, and added new property 'wing displacement'

--- wing.py
+++ wing.py
@@ -1,7 +1,7 @@
 import bpy, math
 import numpy as np
 
-def addWing(delta, chi_eq, tau_points, zeta_points, washout, washout_displacement, wing_length):
+def addWing(delta, chi_eq, tau_points, zeta_points, washout, washout_displacement, wing_length, wing_displacement):
 
     #Constants
     PIRAD = 3.14159
@@ -72,8 +72,8 @@ def addWing(delta, chi_eq, tau_points, zeta_points, washout, washout_displacemen
 
     #This line actually creates the object
     bpy.ops.mesh.primitive_xyz_function_surface(x_eq=x_equation, y_eq=y_equation, z_eq=z_equation, range_u_min=0, range_u_max=1, range_u_step=32, wrap_u=True, range_v_min=3, range_v_max=wing_length, close_v=True)
+    bpy.ops.mesh.primitive_xyz_function_surface(x_eq="-"+x_equation+"+"+str(wing_displacement), y_eq=y_equation, z_eq="-"+z_equation, range_u_min=0, range_u_max=1, range_u_step=32, wrap_u=True, range_v_min=3, range_v_max=wing_length, close_v=True)
     
-#
 #    User interface
 #
  
@@ -93,9 +93,10 @@ class MESH_OT_primitive_wing_add(bpy.types.Operator):
     washout = FloatProperty(name="Washout", default = 0.4)
     washout_displacement = FloatProperty(name="Washout Displacement", default = 0.2)
     wing_length = FloatProperty(name="Adjust wing length", default =6.0, min = 3.00)
+    wing_displacement = FloatProperty(name="Adjust wing displacement", default = 13.00, min =0.00)
     
     def execute(self, context):
-        ob = addWing(self.delta, self.chi_eq, self.tau_points, self.zeta_points, self.washout, self.washout_displacement, self.wing_length)
+        ob = addWing(self.delta, self.chi_eq, self.tau_points, self.zeta_points, self.washout, self.washout_displacement, self.wing_length, self.wing_displacement)
         #context.scene.objects.link(ob)
         #context.scene.objects.active = ob
         return {'FINISHED'}
@@ -120,4 +121,3 @@ def unregister():
  
 if __name__ == "__main__":
     register()
-
